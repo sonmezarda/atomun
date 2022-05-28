0cf01670c76db9d0e3e0e8e856f818d4a6117c91
Arda Sönmez <ssonmezarda@gmail.com>
2022-05-29 00:26:35 +0300

    project stopped for now-1

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 
 var mun_date_ = new Date(2022, 4, 23, 3, 0);
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 
 
 function get_date_difference(mun_date) {
@@ -68,3 +82,32 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+function openApplication(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openFirstApplication(){
+  var button = document.getElementById("first-tab-id");
+  openApplication(button,"committee-director");
+}
+
+openFirstApplication();
